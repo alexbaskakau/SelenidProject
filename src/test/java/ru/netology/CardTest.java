c544922dec701b90e62872277eaf19d88d1e8c1e
Alex Baskakau <lenovos6000v4@gmail.com>
2023-05-29 16:44:30 +0100

    Working commit

---
+++ src/test/java/ru/netology/CardTest.java
@@ -0,0 +1,67 @@
+package ru.netology;
+import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+
+public class CardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--disable-dev-shm-usage");
+        // options.addArguments("--no-sandbox");
+        // options.addArguments("--headless");
+        // driver = new ChromeDriver(options);
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        // driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Казань");
+       // $("[data-test-id=date] input").click();
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue("15.11.2023");
+        $("[data-test-id=name] input").setValue("Джек Дэниэлс");
+        $("[name='phone'").setValue("+73456789997");
+        $("[data-test-id=agreement").click();
+        $(By.className("button")).click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+    }
