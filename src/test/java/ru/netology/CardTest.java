5f1de28b0329b35b1f8fa801c8c6c287107979c3
Alex Baskakau <lenovos6000v4@gmail.com>
2023-06-01 22:18:00 +0100

    Generate date

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,6 +15,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byXpath;
@@ -24,49 +27,28 @@ import static com.codeborne.selenide.files.DownloadActions.click;
 
 
 public class CardTest {
-    private WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll() {
-        // System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        // ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--disable-dev-shm-usage");
-        // options.addArguments("--no-sandbox");
-        // options.addArguments("--headless");
-        // driver = new ChromeDriver(options);
-        WebDriverManager.chromedriver().setup();
+
+    public String generateDate(int daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    @BeforeEach
-    void setUp() {
-        // driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     void shouldTestSomething() {
 
         open("http://localhost:9999/");
-        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=city] input").setValue("Рязань");
         // $("[data-test-id=date] input").click();
         $("[data-test-id=date] input").doubleClick();
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue("15.11.2023");
-        $("[data-test-id=name] input").setValue("Джек Дэниэлс");
-        $("[name='phone'").setValue("+73456789997");
+        String meetDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(meetDate);
+        $("[data-test-id=name] input").setValue("Джонни Уокер");
+        $("[name='phone'").setValue("+73456784670");
         $("[data-test-id=agreement").click();
         $(By.className("button")).click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Встреча")).shouldHave(Condition.text("Встреча успешно запланирована на " + meetDate),Duration.ofSeconds(15)).shouldBe(visible);
 
     }
 }
