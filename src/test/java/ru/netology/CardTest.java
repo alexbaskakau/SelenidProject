8c4e11331830384687b27cd846b0a759a03b1ac9
Alex Baskakau <lenovos6000v4@gmail.com>
2023-06-02 10:38:59 +0100

    Phone changed

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -45,7 +45,7 @@ public class CardTest {
         String meetDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id=date] input").setValue(meetDate);
         $("[data-test-id=name] input").setValue("Джонни Уокер");
-        $("[name='phone'").setValue("+73409514536");
+        $("[name='phone'").setValue("+73409514575");
         $("[data-test-id=agreement").click();
         $(By.className("button")).click();
         $(withText("Встреча")).shouldHave(Condition.text("Встреча успешно забронирована на " + meetDate),Duration.ofSeconds(15)).shouldBe(visible);
